4cbfb7220659bfd0e134fe2dde3d4b1be8a7007e
Atilla <atillafacebook@hotmail.fr>
2019-11-27 10:39:29 +0100

    aftercreate cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
     after_create :welcome_send
+    after_create :set_cart
 
   has_one :cart
 
 def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+
+def set_cart
+  @cart = Cart.create(user_id: self.id)
+end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
